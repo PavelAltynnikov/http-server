ef843c5f40d540f29e17444dfc4c5600591f35c8
PavelAltynnikov <Robenzon11@gmail.com>
2021-06-08 01:01:20 +0400

    remove some hardcode

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 import socket
 
 
 HOST = 'localhost'
 PORT = 8080
-DEFAULT_HEADER = 'HTTP/1.1 '
-OK = '200 OK'
-NOT_FOUND = '404 NotFound'
 
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1024)
@@ -14,7 +12,7 @@ server_sock.bind((HOST, PORT))
 server_sock.listen(10)
 
 
-def print_request():
+def print_request(http_method, path, protocol, headers):
     print(http_method)
     print(path)
     print(protocol, end='\n\n')
@@ -23,35 +21,62 @@ def print_request():
     print()
 
 
-def generate_responce(path) -> bytes:
-    head = ''.encode('utf-8')
+def is_path_exists(path: str) -> bool:
+    if path == r'/':
+        return True
+    return os.path.exists(f'.{path}')
+
+
+def find_source_path(path: str) -> str:
+    if path == r'/':
+        return './static/html/index.html'
+    return f'.{path}'
+
+
+def choise_content_type(extension: str) -> str:
+    if extension in ('html', 'css', ''):
+        return 'text'
+    return 'image'
+
+
+def generate_responce(path: str) -> bytes:
+    starting_line = b'HTTP/1.1 '
+    headers = ''.encode('utf-8')
     body = ''.encode('utf-8')
 
-    if path == r'/':
-        head = b'HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n'
-        with open('./static/html/index.html', 'r') as f:
-            body = f.read().encode('utf-8')
-    elif path == r'/static/css/style.css':
-        head = b'HTTP/1.1 200 OK\nContent-Type: text/css; charset=utf-8\n\n'
-        with open('./static/css/style.css', 'r') as f:
-            body = f.read().encode('utf-8')
-    elif path == r'/static/img/logo/logo_black.png':
-        with open('./static/img/logo/logo_black.png', 'rb') as f:
-            img = f.read()
+    if is_path_exists(path):
+        starting_line += b'200 OK\n'
 
-        head = 'HTTP/1.1 200 OK\n' \
-               'Content-Type: image/png\n' \
-               'Accept-Ranges: bytes\n' \
-               f'Content-length: {len(img)}\n' \
-               f'Location: https://localhost:8000/static/img/logo/logo_black.png\n\n'
-        head = head.encode('utf-8')
-        body = img
+        extension = os.path.splitext(path)[1].strip('.')
+        if extension or path == r'/':
+            if not extension:
+                extension = 'html'
+            source_path = find_source_path(path)
+            content_type = choise_content_type(extension)
+            headers += f'Content-Type: {content_type}/{extension}'.encode('utf-8')
+            if content_type == 'text':
+                headers += b'; charset=utf-8\n'
+            else:
+                headers += b'\n'
+
+            if content_type == 'text':
+                with open(source_path, 'r') as f:
+                    body = f.read().encode('utf-8')
+            else:
+                with open(source_path, 'rb') as f:
+                    img = f.read()
+
+                headers += 'Accept-Ranges: bytes\n' \
+                           f'Content-length: {len(img)}\n' \
+                           f'Location: https://localhost:8000{path}\n'.encode('utf-8')
+                body = img
     else:
-        head = 'HTTP/1.1 404 NotFound\nContent-Type: text/html; charset=utf-8\n\n' \
-            .encode('utf-8')
+        starting_line += b'404 NotFound\n'
         body = 'Сори, но страница не найдена'.encode('utf-8')
 
-    responce = b' '.join((head, body))
+    headers += b'\n'
+    print(b''.join((starting_line, headers)), end='\n\n')
+    responce = b''.join((starting_line, headers, body))
     return responce
 
 
@@ -62,12 +87,11 @@ try:
         request = client.recv(1024).decode('utf-8').strip().split('\r\n')
 
         starting_line = request[0].split()
-        http_method = starting_line[0]
-        path = starting_line[1]
-        protocol = starting_line[2]
+        http_method, path, protocol = starting_line
         headers = {line.split(': ')[0]: line.split(': ')[1] for line in request[1:]}
 
-        print_request()
+        # print_request(http_method, path, protocol, headers)
+        print(path)
 
         if http_method != 'GET':
             client.shutdown(socket.SHUT_RDWR)
